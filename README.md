2e7149f69223febc1e9949523582103a03f15719
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-04-02 19:30:03 +0100

    Start.NG JS Task-2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # js-task1
-Start.NG JavaScript Task 1
 
-# This is a program that displays my name, courses offered at Start.NG and a display of even or odd numbers between 1 and 200 depending on the number of courses offered. 
+#Start.NG JavaScript Task 2
+
+# This is a program that checks for certain rates according to different principals and times and returns the interest on 4 objects. 
---
+++ task-2.js
@@ -0,0 +1,30 @@
+//This is the Start.NG JavaScript Task 2
+
+const data = [{ principal: 2500, time: 1.8 }, { principal: 1000, time: 5 }, { principal: 3000, time: 1 }, { principal: 2000, time: 3 },];
+
+let rate = 0;
+let interest = 0;
+let interestData = [];
+
+function interestCalculator(array) {
+    array.forEach((item) => {
+        if (item.principal >= 2500 && item.time > 1 < 3)
+            rate = 3;
+        else if (item.principal >= 2500 && item.time >= 3)
+            rate = 4;
+        else if (item.principal < 2500 || item.time <= 1)
+            rate = 3;
+        else
+            rate = 1;
+        interest = (item.principal * rate * item.time) / 100;
+        interestData.push({
+            principal: item.principal,
+            time: item.time,
+            rate,
+            interest
+        });
+    });
+    console.log(interestData);
+    
+};
+interestCalculator(data);
