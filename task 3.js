489d97928e906317b19e65a206d3abac2d019a01
Ikenna Oyiih <61995586+I-GiM@users.noreply.github.com>
2020-04-18 22:24:16 +0100

    JavaScript Task 3

---
+++ task 3.js
@@ -0,0 +1,25 @@
+let compile = [];
+Sort = (arg) => {
+    for (i=1; i<=arg; i++) {
+        if (i%2===0 && i%3===0 && i%5===0) {
+            compile.push('yu-gi-oh');
+        } else if (i%2===0 && i%3===0) {
+            compile.push('yu-gi');
+        } else if (i%2===0 && i%5===0) {
+            compile.push('yu-oh');
+        } else if (i%3===0 && i%5===0) {
+            compile.push('gi-oh');
+        } else if (i%2===0) {
+            compile.push('yu');
+        } else if (i%3===0) {
+            compile.push('gi');
+        } else if (i%5===0) {
+            compile.push('oh')
+        } else {
+            compile.push(i);
+        }
+    }
+    console.log(compile);
+}
+Sort(20);
+Sort(100);
